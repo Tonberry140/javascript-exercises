c94da9482f39b012976a03984d9c54efa4bda6c5
tonberry140 <tonberry140@gmail.com>
2022-02-13 18:27:56 +0900

    First completed up to 06

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(originalArray, ...args) {
+    
+for (let arg of args) {
+    
+    if(originalArray.indexOf(arg) > -1) {
+      
+        originalArray.splice(originalArray.indexOf(arg), 1)
+    }
+}
+return originalArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(...inputs) {
+    
+for(let input of inputs) {
+    if(input < 0 || typeof input != "number") {
+        return 'ERROR';
+    }
+}
+    let sortedInput = inputs.sort();
+    let firstNumber = sortedInput[0];
+    let secondNumber = sortedInput[1];
 
+let total = 0;
+for(let i = firstNumber; i <= secondNumber; i++) {
+total += i;
+}
+return total;
 };
 
 // Do not edit below this line
