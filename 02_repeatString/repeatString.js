531f3e1308d44c787400293425906d4c06c5278b
tonberry140 <tonberry140@gmail.com>
2022-02-13 15:30:45 +0900

    first three exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(toRepeat,howManyTimes) {
+    let outputString = ``;
+
+    if (howManyTimes < 0) { return "ERROR";}
+  
+
+    for(let i=0;i < howManyTimes;i++) {
+        outputString += toRepeat;
+    }
+        return outputString
 
 };
+
+console.log(repeatString("hi", 4))
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function(a) {
+let characters = a.split("");
+let wordOutput =[];
+for(let i = characters.length; i > -1; i--) {
+wordOutput.push(characters[i]);
+}
+console.log(wordOutput);
+return wordOutput.join("");
+}
+reverseString("pizza");
 // Do not edit below this line
 module.exports = reverseString;
